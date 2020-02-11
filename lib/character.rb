657c77f5ab03680f00b90d1b0057573b9a446dce
Alex Latham <alexlthm@gmail.com>
2020-02-11 09:07:27 -0700

    Add defi attributes and test

--- lib/character.rb
+++ lib/character.rb
@@ -1,6 +1,10 @@
 class Character
+  attr_reader :name, :actor, :salary
 
 def initialize(character_data_parameter)
+  @name = character_data_parameter[:name]
+  @actor = character_data_parameter[:actor]
+  @salary = character_data_parameter[:salary]
 end
 
 end
