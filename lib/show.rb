0a2647494fb2dd27aa7d7ac7c76d5903c1ba0411
Alex Latham <alexlthm@gmail.com>
2020-02-11 09:12:58 -0700

    Add defi attributes and test for show class

---
+++ lib/show.rb
@@ -0,0 +1,8 @@
+class Show
+
+  def initialize(name_parameter, creator_parameter, actors_parameter)
+    @name = name_parameter
+    @creator = creator_parameter
+    @characters = actors_parameter
+  end
+end
