61c0e7744be62c3b8fa8dbe87641d271aef30258
Alex Latham <alexlthm@gmail.com>
2020-02-11 09:19:34 -0700

    Rename actors param to characters param in defi, add test for highest paid actor

--- lib/show.rb
+++ lib/show.rb
@@ -1,9 +1,13 @@
 class Show
 attr_reader :name, :creator, :characters
 
-  def initialize(name_parameter, creator_parameter, actors_parameter)
+  def initialize(name_parameter, creator_parameter, characters_parameter)
     @name = name_parameter
     @creator = creator_parameter
-    @characters = actors_parameter
+    @characters = characters_parameter
+  end
+
+  def total_salary
+    @characters.reduce(0) { |salaries, character| salaries + character.salary }
   end
 end
